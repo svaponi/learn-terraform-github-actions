519bfbcc931b245683738f06e40984c0f72af4de
svaponi <10941963+svaponi@users.noreply.github.com>
2024-06-28 19:47:36 +0200

    task: replace aws with gcp

--- main.tf
+++ main.tf
@@ -2,73 +2,57 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  backend "gcs" {
+  }
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+    google = {
+      source  = "hashicorp/google"
+      version = "4.79.0"
     }
   }
   required_version = ">= 1.1.0"
 }
 
-provider "aws" {
-  region = "us-west-2"
+provider "google" {
+  project = "sawblade"
+  region  = "europe-west1"
 }
 
-resource "random_pet" "sg" {}
+resource "google_compute_instance" "webserver" {
+  name         = "web-server-instance"
+  machine_type = "e2-micro"
+  zone         = "europe-west1-b"
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+  tags = ["webserver"]
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  network_interface {
+    network = "default"
+    access_config {}
   }
 
-  owners = ["099720109477"] # Canonical
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install -y nginx && echo 'Hello, World!' > /var/www/html/index.nginx-debian.html"
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+resource "google_compute_firewall" "allow-http-to-webserver" {
+  name    = "allow-http"
+  network = "default"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  target_tags = ["webserver"]
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  value = "http://${google_compute_instance.webserver.network_interface.0.access_config.0.nat_ip}"
 }
